0a9c4368e2dddafed7ee6ede8795964d6099cf3f
xu_xz <xuxiaozhong@cptgroup.cn>
2024-03-20 22:01:29 +0800

    feat: 添加tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <button className='bg-red-500 text-white'>测试</button>
     </>
   )
 }
